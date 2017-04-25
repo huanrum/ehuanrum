d3d1ff2b3825e19e57eecd3199a3667357d11d90
seto.sun <seto.sun@sinodynamic.com>
2017-04-25 18:06:00 +0800

    add function

--- build/demo-mall/demo.js
+++ build/demo-mall/demo.js
@@ -2,7 +2,7 @@
 
 $ehr('global',function(){
     return {
-        service:'http://192.168.1.248:8888/mall',
+        service:'http://192.168.1.152:8888/mall',
         update:function(data,user){
             var self = this;
             Object.keys(data||{}).forEach(function(k){
@@ -15,16 +15,45 @@ $ehr('global',function(){
         user:localStorage['ehuanrum_user']
     }
 });
-$ehr('http', ['global',function (global) {
+$ehr('http', ['global', 'binding', function (global, binding) {
     return function (url, parms) {
         var fullUrl = initParms(url, parms);
+
+        var loading = binding([
+            '<div class="common-dialog-back">',
+            '   <div class="common-dialog">',
+            '      <strong [innerHTML]="loadMessage + \'(\' +floor(remaining/2)+ \'s)\' + index" [hidden]="!remaining"></strong>',
+            '      <span [hidden]="!!remaining">加载已经超时</span>',
+            '  </div>',
+            '</div>',
+        ].join(''), { loadMessage: '加载中', index: '...', remaining: 2*30,floor:function(i){return Math.floor(i);} }, document.body, function (scope) {
+            var index = 0;
+            var hald = setInterval(function () {
+                index = (index + 1) % 16;
+                scope.index = Array(index + 4).join('.');
+                scope.remaining = scope.remaining - 1;
+                if (!Math.floor(scope.remaining)) {
+                    setTimeout(function () {
+                        loading.update();
+                    }, 3 * 1000);
+                }
+            }, 500);
+            scope.$destroy(function () {
+                clearInterval(hald);
+            });
+        });
+
+
+
         return new Promise(function (resolve, reject) {
-            if(!fullUrl){
-                reject({message:'参数不完整'});
+            if (!fullUrl) {
+                reject({ message: '参数不完整' });
             }
             fetch(global.service + fullUrl).then(function (response) {
+                loading.update();
                 return response.json();
-            },function(err){
+            }, function (err) {
+                loading.update();
                 reject(err);
             }).then(function (json) {
                 resolve(json);
@@ -33,19 +62,19 @@ $ehr('http', ['global',function (global) {
     };
 
 
-    function initParms(url,parms) {
+    function initParms(url, parms) {
         var parmsStr = Object.keys(parms).sort(function (a, b) {
-                return /:/.test(b.name) - /:/.test(a.name);
-            }).map(function(key){
-            if(url.indexOf(':'+key) !== -1){
-                url = url.replace(':' + key,JSON.parse(parms[key]));
-            }else{
+            return /:/.test(b.name) - /:/.test(a.name);
+        }).map(function (key) {
+            if (url.indexOf(':' + key) !== -1) {
+                url = url.replace(':' + key, JSON.parse(parms[key]));
+            } else {
                 return key + '=' + JSON.parse(parms[key]);
             }
-        }).filter(function(i){return !!i;}).join('&');
+        }).filter(function (i) { return !!i; }).join('&');
 
-        if(url.indexOf(':') === -1){
-            return url + (parmsStr?('?'+parmsStr):'')
+        if (url.indexOf(':') === -1) {
+            return url + (parmsStr ? ('?' + parmsStr) : '')
         }
     }
 
--- demo-mall/app/globel.js
+++ demo-mall/app/globel.js
@@ -2,7 +2,7 @@
 
 $ehr('global',function(){
     return {
-        service:'http://192.168.1.248:8888/mall',
+        service:'http://192.168.1.152:8888/mall',
         update:function(data,user){
             var self = this;
             Object.keys(data||{}).forEach(function(k){
