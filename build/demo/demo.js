eafe6e5b15e8622bf5f8208169fb0a18f171e3e5
seto.sun <seto.sun@sinodynamic.com>
2017-04-25 14:47:45 +0800

    update random

--- build/demo/demo.js
+++ build/demo/demo.js
@@ -678,7 +678,7 @@ window.$ehr('common.drag', [function () {
                 '    <br>',
                 '    <div [item:items] style="display:inline-table;margin:1em;">',
                 '       <div [innerHTML]="item.info"></div>',
-                '       <textarea [value]="item.model" [onkeyup]="random(item)" style="width:300px;height:200px;"></textarea>',
+                '       <textarea [value]="item.model" [onkeyup]="random(item,$element)" style="width:300px;height:200px;"></textarea>',
                 '       <br>',
                 '       <button [onclick]="random(item)">random</button>&nbsp;<button [onclick]="copy(item)">copy</button>',
                 '       <div [innerHTML]="item.value" style="width:300px;word-wrap:break-word;"></div>',
@@ -689,16 +689,19 @@ window.$ehr('common.drag', [function () {
                     name: name,
                     items: [
                         { model: '"[1970-2020]-[1-12]-[1-30] [(AP)1]M [0-12]:[0-59]:[0-59]"', info: '时间' },
-                        { model: '"Name-[(a-z)3-5]"',length:'3-7', info: '字符串数组' },
-                        { model: '{"name":"data-[(0-9)8]","list:3-6":{"id":"[(0-9)1-4]","index":"[0-999]","type":"[(a-Z)3-6]","other[()1-5]":"[()3-7]"}}', info: '对象(里面带有数组)' },
+                        { model: '"Name-[(a-z)3-5]"', length: '3-7', info: '字符串数组' },
+                        { model: '{"name":"data-[(0-9)8]","list:3-6":{"id":"[1+2]","index":"[0-999]","type":"[(a-Z)3-6]","other[()1-5]":"[()3-7]"}}', info: '对象(里面带有数组)' },
                         { model: '"[(0-9)]"', info: '数字,任意位数' }
                     ],
-                    random: function (item) {
+                    random: function (item, element) {
                         if (item.model) {
                             if (this.items[this.items.length - 1] === item) {
                                 this.items = this.items.concat([{}]);
+                                if (element && document.activeElement !== element) {
+                                    element.focus();
+                                }
                             }
-                            item.value = JSON.stringify(random(JSON.parse(item.model),item.length));
+                            item.value = JSON.stringify(random(JSON.parse(item.model), item.length));
                         }
                     },
                     copy: function (item) {
@@ -711,7 +714,7 @@ window.$ehr('common.drag', [function () {
                     }
                 }, function (scope) {
                     scope.items.forEach(function (item) {
-                        item.value = JSON.stringify(random(JSON.parse(item.model),item.length));
+                        item.value = JSON.stringify(random(JSON.parse(item.model), item.length));
                     });
                 });
 
