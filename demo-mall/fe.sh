8a4828f4cb7eca662216e1f50733397fe20f3e99
seto.sun <seto.sun@sinodynamic.com>
2017-06-07 08:57:51 +0800

    update fe.sh

--- demo-mall/fe.sh
+++ demo-mall/fe.sh
@@ -1,1 +1,24 @@
+
+NOCOLOR='\033[0m'
+REDCOLOR='\033[37;41m'
+GREENCOLOR='\033[34;42m'
+
+DIR=`pwd`
+NAME=$(basename "$DIR")
+TITLE="{headtitle}"
+
+cp "$DIR/../index.template" "$DIR/index.template"
+sed -i "s/$TITLE/$NAME/g" "$DIR/index.template"
+echo -e "$GREENCOLOR copy index.html.$NOCOLOR"
+echo -e " "
+
+cp "$DIR/../gulpfile.js" "$DIR/gulpfile.js"
+echo -e "$GREENCOLOR copy gulpfile.js.$NOCOLOR"
+echo -e " "
+
+echo -e "$REDCOLOR gulp.$NOCOLOR"
+echo -e " "
 gulp
+
+rm -f "$DIR/gulpfile.js"
+rm -f "$DIR/index.template"
