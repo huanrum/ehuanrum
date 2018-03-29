3c6988d88b8370e224d7d41885e0fb37697a1f8b
seto.sun <seto.sun@sinodynamic.com>
2018-03-29 18:18:26 +0800

    update

--- build/demo/demo.js
+++ build/demo/demo.js
@@ -581,6 +581,39 @@ window.$ehr('common.drag', [function () {
     'use strict';
 
     //界面上的菜单数据以及路由和界面,必须以router.开头
+    $e('router.learn', ['common_page','service_learn',function (common_page,service_learn) {
+
+        return function (name) {
+            return common_page([
+               '<div [my.grid]="items"></div>'
+            ].join(''),{
+                    title:'Learn',
+                    items:service_learn.get(),
+                    select:function(){
+                        service_learn.select(this.item);
+                    }
+            });
+        };
+    }]);
+
+     $e('service.learn', ['common_service',function (common_service) {
+
+        var service = common_service({fields:['id','name','value','date']});
+        service.load();
+        return service;
+
+    }]);
+
+
+})(window.$ehr);
+
+/**
+ * Created by Administrator on 2017/3/28.
+ */
+(function ($e) {
+    'use strict';
+
+    //界面上的菜单数据以及路由和界面,必须以router.开头
     $e('router.work.binding', ['common_page', 'common_dialog', 'functions','random', function (common_page, common_dialog, functions,random) {
 
         return function (name) {
@@ -706,39 +739,6 @@ window.$ehr('common.drag', [function () {
 
             return binding;
         };
-    }]);
-
-
-})(window.$ehr);
-
-/**
- * Created by Administrator on 2017/3/28.
- */
-(function ($e) {
-    'use strict';
-
-    //界面上的菜单数据以及路由和界面,必须以router.开头
-    $e('router.learn', ['common_page','service_learn',function (common_page,service_learn) {
-
-        return function (name) {
-            return common_page([
-               '<div [my.grid]="items"></div>'
-            ].join(''),{
-                    title:'Learn',
-                    items:service_learn.get(),
-                    select:function(){
-                        service_learn.select(this.item);
-                    }
-            });
-        };
-    }]);
-
-     $e('service.learn', ['common_service',function (common_service) {
-
-        var service = common_service({fields:['id','name','value','date']});
-        service.load();
-        return service;
-
     }]);
 
 
--- framework/ehuanrum.base.js
+++ framework/ehuanrum.base.js
@@ -348,7 +348,7 @@
     function $value($obj, $field, $value) {
         if (!/^[0-9a-zA-Z\._$@]*$/.test($field)) {
             //计算表达式
-            return _eval($obj, $field, valuer);
+            return _eval($obj, $field.replace(/\s+/mg,' '), valuer);
         } else {
             //取值
             return valuer($obj, $field, $value);
@@ -828,7 +828,7 @@
 
             function extendArray(list) {
                 if (list instanceof Array) {
-                    ['push', 'pop', 'shift', 'unshift'].forEach(function (funName) {
+                    ['push', 'pop', 'shift', 'unshift','sort'].forEach(function (funName) {
                         Object.defineProperty(list, funName, {
                             configurable: true,
                             value: function () {
@@ -838,19 +838,30 @@
                             }
                         });
                     });
+                    Object.defineProperty(list, 'replace', {
+                        configurable: true,
+                        value: function () {
+                            list.length = 0;
+                            Array.prototype.forEach.call(arguments,function(arg){
+                                Array.prototype.push.apply(list, arg||[]);
+                            });
+                            render(list);
+                            return list;
+                        }
+                    });
                 }
                 return list;
             }
 
             function render(vals) {
-                elements.forEach(function (it) {
-                    if (!Array.prototype.some.call(vals,function (i) { return it.t === i; })) {
+                elements.forEach(function (it,index) {
+                    if(vals[index] !== it.t){
                         it.e.update();
                     }
                 });
                 elements = map(vals || [], function (item, i, obj, da) {
-                    var bindElement = (elements.filter(function (it) { return it.t === item; })[0] || {}).e;
-                    if (!bindElement) {
+                    var bindElement = (elements.filter(function (it) { return it.t === item; })[0] || { t: item, e: bindElement });
+                    if (!bindElement.e || bindElement.e.scope().item !== bindElement.t) {
                         if (typeof i === 'number') {
                             Object.defineProperty(da, '$index', {
                                 enumerable: false,
@@ -870,14 +881,15 @@
                             }
                         });
                         da.__proto__ = data;
-                        bindElement = binding(outerHTML, da);
-                        data.$eval(da.$eval);
+                        bindElement.e = binding(outerHTML, da);
+                        bindElement.e.update(parentNode, nextSibling);
+                        data.$eval(function(){da.$eval.apply(da,arguments);});
                     } else {
-                        bindElement.scope().$eval();
+                        bindElement.e.update(parentNode);
+                        bindElement.e.scope().$eval();
                     }
-                    bindElement.update(parentNode, nextSibling);
 
-                    return { t: item, e: bindElement };
+                    return bindElement;
                 });
 
                 function map(obj, fn) {
